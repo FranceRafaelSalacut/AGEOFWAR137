c67f350b429c0e0257c92275dba5adb771dfb982
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2023-11-21 02:20:07 +0800

    button now clickable

--- gameClasses/button.py
+++ gameClasses/button.py
@@ -7,18 +7,29 @@ def render(text):
 
 class Button():
     def __init__(self, text, color, pos_x, pos_y, height, width):
-        self.text = render(text)
+        self.text = text
         self.color = color
         self.position = (pos_x, pos_y)
         self.size = (height, width)
         self.rect = pygame.Rect(self.position, self.size)
+        self.clicked = False
 
     def draw(self, screen,  ):
+        on_top = False
         #Drawing the colored rectangle on the screen
         pygame.draw.rect(screen, self.color, self.rect )
 
         #Drawing the text over the rectangle
-        screen.blit(self.text, (self.position))
+        screen.blit(render(self.text), (self.position))
 
-        print(self.position[0])
+        #Tracking the mouse movement
+        mouse = pygame.mouse.get_pos()
 
+        #Setting up button if clicked functionality
+        if self.rect.collidepoint(mouse): 
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                print("Here on " + str(self.text))
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
