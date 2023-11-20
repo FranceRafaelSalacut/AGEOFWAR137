51a6507cedfa973a9c7426d5cf4ce2d4428d18cc
riaqliu <riaqliu@gmail.com>
2023-11-21 00:39:58 +0800

    Added more content for client

--- client.py
+++ client.py
@@ -1,19 +1,33 @@
 import pygame
+from CONSTANTS import *
 
+
+# pygame setup
 pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 640
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('TEST')
-
+clock = pygame.time.Clock()
+pygame.display.set_caption('TEST') # game/window title
+dt = 0 # deltaTime
 run = True
 
 while run:
-    screen.fill((0, 0, 0))
+    # polling for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # filling the screen with a color to wipe away everything from the last frame
+    screen.fill('white')
+
+    # Render game here
+    # vv===========================================vv
+
+
+
+    # ^^===========================================^^
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000 # limits FPS to 60
 
 pygame.quit()
