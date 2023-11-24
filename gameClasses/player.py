25f07a071a5d0d67525b484806da459d9757d1b7
Johannes <zberzerker246@gmail.com>
2023-11-24 15:24:10 +0800

    added player and projecile class

---
+++ gameClasses/player.py
@@ -0,0 +1,7 @@
+import pygame as pg
+
+class player():
+    def __init__(self):
+        pass
+    def train(self):
+        pass
---
+++ gameClasses/projectile.py
@@ -0,0 +1,5 @@
+import pygame as pg
+
+class projectile(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
