60e517de7a243e067aed5e561b16ce6b3fcf2766
Johannes <zberzerker246@gmail.com>
2023-11-24 14:22:09 +0800

    updated baseBase

--- gameClasses/baseBase.py
+++ gameClasses/baseBase.py
@@ -1,2 +1,8 @@
-class baseBase():
-    pass
+import pygame as pg
+
+class baseBase(pg.sprite.Sprite):
+    def __init__(self, id, hp):
+        pg.sprite.Sprite.__init__(self)
+        self.id = id
+        self.hp = hp # Hit points
+        
--- gameClasses/baseUnit.py
+++ gameClasses/baseUnit.py
@@ -1,16 +1,21 @@
 import pygame as pg
 vec = pg.math.Vector2
 
+# TODO: Add image and rect attributes (different per unit)
+
 class baseUnit(pg.sprite.Sprite):
-    def __init__(self, id, hp, mspd, aspd, arng, dmg, bounty):
+    def __init__(self, id, hp, mspd, aspd, arng, dmg, cost, bounty):
         pg.sprite.Sprite.__init__(self)
         self.id = id
-        self.hp = hp # Health points
+        self.hp = hp # Hit points
         self.mspd = mspd # Movement speed
         self.aspd = aspd # Attack speed
         self.arng = arng # Attack range
         self.dmg = dmg # Damage
+        self.cost = cost # Gold required to purchase the unit
         self.bounty = bounty # Gold dropped when unit dies
         self.pos = vec(0,0) # Unit position
     def attack(self):
+        pass
+    def die(self):
         pass
