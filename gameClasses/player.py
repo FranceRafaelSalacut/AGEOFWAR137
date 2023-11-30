aee7e831e19a1f8b239cb2bdb050e518f1fb24dd
Johannes <zberzerker246@gmail.com>
2023-11-30 17:08:02 +0800

    placeholder methods for Player class

--- gameClasses/player.py
+++ gameClasses/player.py
@@ -1,7 +1,14 @@
 import pygame as pg
 
-class player():
+class Player():
     def __init__(self):
         pass
     def train(self):
         pass
+    def upgrade(self):
+        pass
+    def send(self, unit, player):
+        pass
+    def receive(self, unit, player):
+        pass
+    
