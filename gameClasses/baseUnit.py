c7322c4ded48a662ebe38f8079fa618e38a32f07
Johannes <zberzerker246@gmail.com>
2023-11-30 15:35:04 +0800

    updated baseUnit as an abstract class

--- gameClasses/baseUnit.py
+++ gameClasses/baseUnit.py
@@ -1,21 +1,12 @@
-import pygame as pg
-vec = pg.math.Vector2
+from abc import ABC, abstractmethod
 
-# TODO: Add image and rect attributes (different per unit)
-
-class baseUnit(pg.sprite.Sprite):
-    def __init__(self, id, hp, mspd, aspd, arng, dmg, cost, bounty):
-        pg.sprite.Sprite.__init__(self)
-        self.id = id
-        self.hp = hp # Hit points
-        self.mspd = mspd # Movement speed
-        self.aspd = aspd # Attack speed
-        self.arng = arng # Attack range
-        self.dmg = dmg # Damage
-        self.cost = cost # Gold required to purchase the unit
-        self.bounty = bounty # Gold dropped when unit dies
-        self.pos = vec(0,0) # Unit position
+class baseUnit(ABC):
+    @abstractmethod
+    def move(self):
+        pass
+    @abstractmethod
     def attack(self):
         pass
+    @abstractmethod
     def die(self):
         pass
