1ac5e855e80b106f20bebd09ce428d37ffb819fa
Johannes <zberzerker246@gmail.com>
2023-12-05 20:13:17 +0800

    added direction and group attributes for melee units. Also added some movement

--- gameClasses/meleeUnit.py
+++ gameClasses/meleeUnit.py
@@ -1,8 +1,9 @@
 from gameClasses.baseUnit import baseUnit
 import pygame as pg
+from CONSTANTS import *
 
 
-class meleeUnit(baseUnit):
+class MeleeUnit(baseUnit):
     def __init__(self, id, x, y, width, height):
         self.id = id
         self.x = x
@@ -16,58 +17,99 @@ class meleeUnit(baseUnit):
     def die(self):
         pass
 
-class Caveman(pg.sprite.Sprite, meleeUnit):
-    def __init__(self, id, x, y, width, height):
+class Caveman(pg.sprite.Sprite, MeleeUnit):
+    def __init__(self, id, x, y, width, height, direction, groups):
         super(Caveman, self).__init__()
+        pg.sprite.Sprite.__init__(self, groups) # Init the sprit within this group
+        self.groups = groups
         self.image = pg.Surface([width, height])
-        self.image.fill((0,0,0))
+        self.image.fill((255,0,0))
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
+        self.direction = direction
         self.hp = 0
-        self.mspd = 0
+        self.mspd = 2
         self.aspd = 0
         self.dmg = 0
         self.bounty = 0
         self.exp = 0
 
-class Footman(pg.sprite.Sprite, meleeUnit):
-    def __init__(self, id, x, y, width, height):
+    def update(self):
+        self.rect.centerx += self.mspd
+        # 
+        if self.rect.left > SCREEN_WIDTH + 100:
+            self.kill()
+        else:
+            print(self.rect.centerx)
+
+class Footman(pg.sprite.Sprite, MeleeUnit):
+    def __init__(self, id, x, y, width, height, direction, groups):
         super(Footman, self).__init__()
+        pg.sprite.Sprite.__init__(self, groups) # Init the sprit within this group
+        self.groups = groups
         self.image = pg.Surface([width, height])
         self.image.fill((0,0,0))
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
         self.hp = 0
-        self.mspd = 0
+        self.mspd = 2
         self.aspd = 0
         self.dmg = 0
         self.bounty = 0
         self.exp = 0
+        
+    def update(self):
+        self.rect.centerx += self.mspd
+        # 
+        if self.rect.left > SCREEN_WIDTH + 100:
+            self.kill()
+        else:
+            print(self.rect.centerx)
 
-class Soldier(pg.sprite.Sprite, meleeUnit):
-    def __init__(self, id, x, y, width, height):
+class Soldier(pg.sprite.Sprite, MeleeUnit):
+    def __init__(self, id, x, y, width, height, direction, groups):
         super(Soldier, self).__init__()
+        pg.sprite.Sprite.__init__(self, groups) # Init the sprit within this group
+        self.groups = groups
         self.image = pg.Surface([width, height])
         self.image.fill((0,0,0))
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
         self.hp = 0
-        self.mspd = 0
+        self.mspd = 2
         self.aspd = 0
         self.dmg = 0
         self.bounty = 0
         self.exp = 0
+        
+    def update(self):
+        self.rect.centerx += self.mspd
+        # 
+        if self.rect.left > SCREEN_WIDTH + 100:
+            self.kill()
+        else:
+            print(self.rect.centerx)
 
-class Robot(pg.sprite.Sprite, meleeUnit):
-    def __init__(self, id, x, y, width, height):
+class Robot(pg.sprite.Sprite, MeleeUnit):
+    def __init__(self, id, x, y, width, height, direction, groups):
         super(Robot, self).__init__()
+        pg.sprite.Sprite.__init__(self, groups) # Init the sprit within this group
+        self.groups = groups
         self.image = pg.Surface([width, height])
         self.image.fill((0,0,0))
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
         self.hp = 0
-        self.mspd = 0
+        self.mspd = 2
         self.aspd = 0
         self.dmg = 0
         self.bounty = 0
         self.exp = 0
+        
+    def update(self):
+        self.rect.centerx += self.mspd
+        # 
+        if self.rect.left > SCREEN_WIDTH + 100:
+            self.kill()
+        else:
+            print(self.rect.centerx)
