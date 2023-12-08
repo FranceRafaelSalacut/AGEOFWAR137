68fd660f5615eae883eb935d3a76062b04a1cf00
Johannes <zberzerker246@gmail.com>
2023-12-08 18:38:04 +0800

    added hpBar class

---
+++ gameClasses/hpBar.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+class hpBar(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+    def update(self):
+        pass
