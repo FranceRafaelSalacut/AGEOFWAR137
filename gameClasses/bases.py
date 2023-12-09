0ca3a789d6602c5d35670f07771ff259f8eadd85
Johannes <zberzerker246@gmail.com>
2023-12-09 19:17:19 +0800

    updated base attributes and drawing hp rects

--- gameClasses/bases.py
+++ gameClasses/bases.py
@@ -2,18 +2,20 @@ import pygame as pg
 from gameClasses.baseBase import baseBase
 
 class prehistoricBase(pg.sprite.Sprite, baseBase):
-    def __init__(self, id, x, y, height, width, groups):
+    def __init__(self, id, x, y, height, width, groups, screen):
         super().__init__(groups)
         self.id = id
         self.x = x
         self.y = y
         self.groups = groups
+        self.screen = screen
         self.image = pg.Surface([width, height])
         self.image.fill((0,0,255))
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]        
         self.width = width
         self.height = height
+        self.hp = 10
     def update(self):
         pg.draw.rect(self.screen, (255,0,0), (self.rect.left, self.rect.top - 20, self.width, 10))
         pg.draw.rect(self.screen, (0,128,0), (self.rect.left, self.rect.top - 20, self.width - (5 * (10 - self.hp)), 10))
