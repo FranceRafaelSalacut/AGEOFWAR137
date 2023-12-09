30618afee8da8d96d6d1f592bbbf21ffc4870efc
Johannes <zberzerker246@gmail.com>
2023-12-09 16:24:25 +0800

    removed hpBar class

--- gameClasses/hpBar.py
+++
@@ -1,9 +0,0 @@
-import pygame as pg
-
-class hpBar(pg.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.x = x
-        self.y = y
-    def update(self):
-        pass
