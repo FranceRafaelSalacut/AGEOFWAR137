7fbac8e067c7680183299c735663adf78e13be60
riaqliu <riaqliu@gmail.com>
2023-12-09 17:54:18 +0800

    added other base types

--- gameClasses/baseBase.py
+++ gameClasses/baseBase.py
@@ -5,3 +5,19 @@ class baseBase(baseModel):
     def __init__(self, id,width, height, x, y):
         super().__init__(id,width, height, x, y)
         self.hp = 0
+
+class Cave(baseBase):
+    def __init__(self, id, width, height, x, y):
+        super().__init__(id, width, height, x, y)
+
+class Castle(baseBase):
+    def __init__(self, id, width, height, x, y):
+        super().__init__(id, width, height, x, y)
+
+class Camp(baseBase):
+    def __init__(self, id, width, height, x, y):
+        super().__init__(id, width, height, x, y)
+
+class Citadel(baseBase):
+    def __init__(self, id, width, height, x, y):
+        super().__init__(id, width, height, x, y)
