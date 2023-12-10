47bdd5dcf8a08243bab7cdfcf877b2ed0368475b
Johannes <zberzerker246@gmail.com>
2023-12-10 16:26:34 +0800

    added die method and hp check in update method

--- gameClasses/baseUnit.py
+++ gameClasses/baseUnit.py
@@ -28,11 +28,18 @@ class baseUnit(baseModel):
         
     def move(self):
         self.movePattern.move()
+        
+    def die(self):
+        #TODO: add death animation here (if we're gonna use em. Also prolly need to put a delay before calling self.kill())
+        self.kill()
     
-    def update(self, screen): 
+    def update(self, screen):
+        self.curhp -= 0.01
         self.hpratio = self.curhp/self.hp       
         pg.draw.rect(screen, (255,0,0), (self.rect.left, self.rect.top - 20, self.rect.width, 10))
         pg.draw.rect(screen, (0,128,0), (self.rect.left, self.rect.top - 20, self.rect.width * self.hpratio, 10))
+        if self.curhp <= 0:
+            self.die()
 
     @abstractmethod
     def attack(self):
