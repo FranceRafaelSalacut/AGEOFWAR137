b207f3f5b37cfeadf8ccbe60bfe707e06ee3ba6f
Johannes <zberzerker246@gmail.com>
2023-12-11 10:42:42 +0800

    updated projectile class

--- gameClasses/projectile.py
+++ gameClasses/projectile.py
@@ -1,25 +1,18 @@
 import pygame as pg
+from gameClasses.baseModel import baseModel
+from .gameClassValues import PROJECTILES
 
-class Projectile(pg.sprite.Sprite):
-    def __init__(self, x, y, vel, direction):
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.vel = vel
-        self.direction = direction
+class Projectile(baseModel):
+    def __init__(self, id, x = 0, y = 0):
+        super().__init__(id, x, y)
+
+    def fetchValues(self, unitType : str):
+        val = PROJECTILES[unitType]
+        # Get the damage value from the rannged unit
+        self.image = pg.image.load(val["img"]).convert_alpha()
+        self.image = pg.transform.scale(self.image, val["imgScale"])
+        self.rect = self.image.get_rect()   
     def draw(self):
         pass
-    
-"""
-class Stone(Projectile):
-    pass
-
-class Arrow(Projectile):
-    pass
-
-class Bullet(Projectile):
-    pass
-
-class Laserbeam(Projectile):
-    pass
-"""
+    def die(self):
+        pass
